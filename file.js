1a7fa4ad3a6c830a81778cda2750da741574c2b3
SyedFazal110 <syedfazal808@gmail.com>
2024-02-08 21:33:26 +0500

    js file created

---
+++ file.js
@@ -0,0 +1,16 @@
+// var a = 20;
+// var b = 20;
+
+// console.log(a )
+
+var yourName = "Syed Fazal";
+var lastName = "Ali Jafferi";
+var age = 23;
+var isUser = 'false'
+
+// typeof()
+// alert()
+
+console.log(yourName +" "+ lastName);
+console.log(age);
+console.log(isUser);
